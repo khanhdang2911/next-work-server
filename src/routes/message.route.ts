dc17a6fc9feba780424d648ceae992fbaf0a777a
Dang Xuan Khanh - 3152 <khanhdang3152@gmail.com>
2025-04-21 16:59:38 +0700

    feat: send message with attach files

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import asyncErrorHandler from '~/helpers/async-error-handler'
 import authMiddleware from '~/middlewares/auth.middleware'
 import * as messageController from '~/controllers/message.controller'
+import uploadCloud from '~/configs/cloudinary.init'
 const messageRouter = Router()
 messageRouter.use(asyncErrorHandler(authMiddleware))
-messageRouter.post('/', asyncErrorHandler(messageController.createMessage))
+messageRouter.post('/', uploadCloud.array('files', 5), asyncErrorHandler(messageController.createMessage))
 export default messageRouter
