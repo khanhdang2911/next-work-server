267d5e232ffd1b8ea2f1958624ea347b0153ed27
Dang Xuan Khanh - 3152 <khanhdang3152@gmail.com>
2025-04-30 15:55:45 +0700

    config: add deploy use docker pipeline

--- src/core/success.response.ts
+++ src/core/success.response.ts
@@ -1,6 +1,8 @@
 import { Response, Request } from 'express'
 import logger from '~/loggers/winston.log'
 import { v4 as uuidv4 } from 'uuid'
+import dotenv from 'dotenv'
+dotenv.config()
 class SuccessResponse {
   public constructor(
     public statusCode: number,
@@ -15,12 +17,14 @@ class SuccessResponse {
       data: this.data,
       options: this.options
     })
-    const req: Request = res.req
-    const traceId = uuidv4()
-    const messageLog = `${traceId} - ${this.statusCode.toString()} - ${this.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`
-    const metadata = req.method === 'POST' ? req.body : req.query
-    logger.info(messageLog)
-    logger.info(`${traceId} - ${JSON.stringify(metadata)}`)
+    if (process.env.ENVIRONMENT !== 'production') {
+      const req: Request = res.req
+      const traceId = uuidv4()
+      const messageLog = `${traceId} - ${this.statusCode.toString()} - ${this.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`
+      const metadata = req.method === 'POST' ? req.body : req.query
+      logger.info(messageLog)
+      logger.info(`${traceId} - ${JSON.stringify(metadata)}`)
+    }
   }
 }
 
